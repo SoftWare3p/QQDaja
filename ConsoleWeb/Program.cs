2227aba0c5918ec93c10d6270710ccc4357340c2
李东珉 <ldm200026@live.cn>
2020-05-10 12:47:29 +0800

    3.0

--- ConsoleWeb/Program.cs
+++ ConsoleWeb/Program.cs
@@ -80,13 +80,13 @@ namespace ConsoleWeb
             }
             private static void theout(object sender, System.Timers.ElapsedEventArgs e)
             {
-                Console.WriteLine("响应时间大于10分钟，可能为账号信息错误");
+                Console.WriteLine("响应时间大于5分钟，可能为账号信息错误");
                 Environment.Exit(0);
             }
             public void daka(string[] str)
             {
                 //initWebVisit();
-                System.Timers.Timer t = new System.Timers.Timer(600000);
+                System.Timers.Timer t = new System.Timers.Timer(300000);
                 //到达时间的时候执行事件；
                 t.Elapsed += new System.Timers.ElapsedEventHandler(theout);
                 t.AutoReset = false;//设置是执行一次（false）还是一直执行(true)； 
